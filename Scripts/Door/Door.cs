b5216d745dc53fe4e0628736a225c9951ebfb15f
Pedro Avelino <39682592+PedroAvelino@users.noreply.github.com>
2019-01-27 15:33:00 -0200

    Scripts

---
+++ Scripts/Door/Door.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+    Door[] availableDoors;
+    public List<Door> doors = new List<Door>();
+
+    int totalDoors;
+
+
+    private void Start()
+    {
+        availableDoors = GameObject.FindObjectsOfType<Door>();
+        totalDoors = availableDoors.Length;
+
+
+        foreach (Door door in availableDoors)
+        {
+            doors.Add(door);
+        }
+
+        for (int i = 0; i < totalDoors; i++)
+        {
+            if (doors[i].Equals(this))
+            {
+                doors.Remove(doors[i]);
+            }
+        }
+        
+    }
+
+    public void TeleportPlayer(Player player)
+    {
+
+        player.transform.position = doors[Random.Range(0, doors.Capacity)].transform.position;
+    }
+}
